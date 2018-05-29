dd4215013550e43da4b21e783377127d2cb397bb
Andries Smit <mr.andries.smit@gmail.com>
2018-05-29 15:39:27 +0200

    Support read only text inputs

--- src/ClipboardTextCopy/widget/ClipboardTextCopy.js
+++ src/ClipboardTextCopy/widget/ClipboardTextCopy.js
@@ -1,108 +1,97 @@
-/*global logger*/
-// Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
-    "dijit/_TemplatedMixin",
-
-    "mxui/dom",
-    "dojo/dom",
-    "dojo/dom-prop",
-    "dojo/dom-class",
-    "dojo/dom-style",
-    "dojo/dom-construct",
-    "dojo/_base/array",
     "dojo/_base/lang",
-    "dojo/text",
-    "dojo/html",
-    "dojo/_base/event",
+    "dojo/dom-construct",
     "dojo/query",
-    
-    "dojo/text!ClipboardTextCopy/widget/template/ClipboardTextCopy.html"
-], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoClass, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoText, dojoHtml, dojoEvent, dojoQuery, widgetTemplate) {
+], function (declare, _WidgetBase, dojoLang, dojoConstruct, dojoQuery) {
     "use strict";
 
-    // Declare widget's prototype.
-    return declare("ClipboardTextCopy.widget.ClipboardTextCopy", [ _WidgetBase, _TemplatedMixin ], {
-        // _TemplatedMixin will create our dom node using this HTML template.
-        templateString: widgetTemplate,
-
-        // DOM elements
-        copyClipboardButtonNode: null,
-
+    return declare("ClipboardTextCopy.widget.ClipboardTextCopy", [ _WidgetBase ], {
         // Parameters configured in the Modeler.
-        labelCaption: "",
         targetClassName: "",
+        labelCaption: "",
+        linkTitle: "",
+        renderType: "button", // or link
+        icon: "",
+        targetClassName: "",
+        buttonstyle: "",
 
-
-        // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-        _handles: null,
-        _contextObj: null,
-        _alertDiv: null,
-        
-        _targetElement: null,
-
-        // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
-        constructor: function() {
-            // Uncomment the following line to enable debug messages
-            //logger.level(logger.DEBUG);
-            logger.debug(this.id + ".constructor");
-            this._handles = [];
-        },
-
-        // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function() {
-            logger.debug(this.id + ".postCreate");
-                        
-            this._setupEvents();
+            if (this.findElement()) {
+                this.updateRendering();
+            } else {
+                var message = "modeler configuration error no element found with class: " + this.targetClassName;
+                this.showError("Clipboard Text Copy widget: " + message);
+                logger.error(this.friendlyId || this.id, message);
+            }
         },
 
-        
-
-        // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
-        update: function(obj, callback) {
-            logger.debug(this.id + ".update");
-
-            callback();
+        updateRendering: function() {
+            var btnStyleClass = this.renderType === "link" ? "" : " btn-" + this.buttonstyle;
+            this.button = new mxui.widget._Button({
+                caption: this.labelCaption,
+                title: this.linkTitle,
+                iconUrl: this.icon,
+                onClick: dojoLang.hitch(this, this.onclickEvent),
+                renderType: this.renderType,
+                cssStyle: this.style,
+                cssClasses: this.classes + btnStyleClass
+            });
+            dojoConstruct.place(this.button.domNode, this.domNode);
         },
 
+        findElement: function() {
+            var target = dojoQuery("." + this.targetClassName + " input")[0];
 
-        // Attach events to HTML dom elements
-        _setupEvents: function() {
-            logger.debug(this.id + "._setupEvents");
+            if (!target) {
+                target = dojoQuery("." + this.targetClassName + " textarea")[0];
+            }
 
-            this.connect(this.copyClipboardButtonNode, "click", dojoLang.hitch(this, function(e) {
-                this._targetElement = dojoQuery("."+this.targetClassName+" input")[0];
-                
-                if(!this._targetElement) {
-                    this._targetElement = dojoQuery("."+this.targetClassName+" textarea")[0];
-                }
-                
-                if (!this._targetElement) {
-                    this._targetElement = dojoQuery("."+this.targetClassName)[0];
-                }
+            if (!target) {
+                target = dojoQuery("." + this.targetClassName + " .form-control-static")[0];
+            }
 
-                if(this._targetElement) {
-                 
-                    this._targetElement.select();
+            if (!target) {
+                target = dojoQuery("." + this.targetClassName)[0];
+            }
+            return target;
+        },
 
-                    try {
-                        var successful = document.execCommand('copy');
-                        var msg = successful ? 'successful' : 'unsuccessful';
-                        console.log('Copying text command was ' + msg);
-                    } catch (err) {
-                        console.log('Oops, unable to copy');
-                    }
-                }
-                else {
-                    console.error('No element found with class: ' + this.targetClassName);
-                }
-            }));
+        onclickEvent: function () {
+            var element = this.findElement();
+
+            if (element) {
+                var value = element.value !== undefined 
+                    ? element.value
+                    : element.innerText;
+                this.copyToClipboard(value);
+            }
+        },
+
+        copyToClipboard: function(text){
+            var dummy = document.createElement("textarea");
+            document.body.appendChild(dummy);
+            dummy.innerHTML = text;
+            dummy.select();
+            document.execCommand("copy");
+            document.body.removeChild(dummy);
+        },
+
+        uninitialize() {
+           if (this.button) {
+               this.button.uninitialize();
+           }
+        },
+
+        showError: function (message) {
+            var alert = dojoConstruct.create("div", {
+                "class": "alert alert-danger",
+                "innerHTML": message
+            });
+            dojoConstruct.place(alert, this.domNode);
         }
-
     });
 });
 
-require(["ClipboardTextCopy/widget/ClipboardTextCopy"], function() {
-    "use strict";
-});
+require(["ClipboardTextCopy/widget/ClipboardTextCopy"]);
